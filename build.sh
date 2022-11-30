99b6bff173c18d07cca251c872d10f4613eeb669
ssanger <blulady@gmail.com>
2022-11-30 11:18:22 -0800

    created build file

---
+++ build.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py makemigrations
+python manage.py migrate
+
+python manage.py collectstatic --noinput
+
+python manage.py shell <<EOF
+
+from django.contrib.auth.models import User
+
+User.objects.create_superuser(
+                username = '$SUPER_USERNAME',
+                email = '$SUPER_EMAIL',
+                password = '$SUPER_PASSWORD'
+)
+
+exit()
+
+EOF

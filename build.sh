f5c4d6407490f870d24aaa776ec61589f3b2929b
ssanger <blulady@gmail.com>
2022-12-01 20:04:36 -0800

    updated build with comments

--- build.sh
+++ build.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -o errexit
+
+echo "Installing python dependencies"
 python -m pip install --upgrade pip
 pip install -r requirements.txt
+
+echo "Running database migrations"
 python manage.py makemigrations
 python manage.py migrate
 
+echo "Collecting staticfiles"
 python manage.py collectstatic --noinput
 
 python manage.py shell <<EOF

e4eecd31549f30c3ebcfac5d18e186357a7c3a66
ssanger <blulady@gmail.com>
2022-12-06 19:18:47 -0800

    new dj_db_url.config()

--- recipes/settings.py
+++ recipes/settings.py
@@ -92,9 +92,7 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", 5432),
     }
 }
-# DATABASES = {
-#     "default": dj_database_url.config(default=os.environ.get('DATABASE_URL', ""))
-# }
+DATABASES = {'default': dj_database_url.config(conn_max_age=97)}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators

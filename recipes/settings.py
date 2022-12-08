3f9e27a8f77ccbe5bf445ab9fdc448e3d63a85c3
ssanger <blulady@gmail.com>
2022-12-08 14:56:10 -0800

    single quotes

--- recipes/settings.py
+++ recipes/settings.py
@@ -83,7 +83,7 @@ WSGI_APPLICATION = "recipes.wsgi.application"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config()
+  'default': dj_database_url.config()
 }
 
 # Password validation

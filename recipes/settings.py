07de7544ee1ac8dc3d539d371e2389543fff3b34
ssanger <blulady@gmail.com>
2022-12-07 20:21:45 -0800

    dj_url

--- recipes/settings.py
+++ recipes/settings.py
@@ -82,16 +82,16 @@ WSGI_APPLICATION = "recipes.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": os.getenv("DB_NAME", "db"),
-#         "USER": os.getenv("DB_USER", "kenny"),
-#         "HOST": os.getenv("DB_HOST", "localhost"),
-#         "PASSWORD": os.getenv("DB_PASSWORD", ""),
-#         "PORT": os.getenv("DB_PORT", 5432),
-#     }
-# }
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME", "db"),
+        "USER": os.getenv("DB_USER", "kenny"),
+        "HOST": os.getenv("DB_HOST", "localhost"),
+        "PASSWORD": os.getenv("DB_PASSWORD", ""),
+        "PORT": os.getenv("DB_PORT", 5432),
+    }
+}
 DATABASES = {
     "default": dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }

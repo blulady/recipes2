93c5d505502e69c2fc1c46161772afaf8411f37e
ssanger <blulady@gmail.com>
2022-12-07 19:38:29 -0800

    dj_db_ur again

--- recipes/settings.py
+++ recipes/settings.py
@@ -93,7 +93,7 @@ WSGI_APPLICATION = "recipes.wsgi.application"
 #     }
 # }
 DATABASES = {
-    "default": dj_database_url.config(os.environ.get('DATABASE_URL'))
+    "default": dj_database_url.config(default=os.environ.get('DATABASE_URL'))
 }
 
 # Password validation
